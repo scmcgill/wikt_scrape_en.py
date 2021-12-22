6084e11225251ed86bb630fd930d2eb7be110098
scmcgill <45632695+scmcgill@users.noreply.github.com>
2021-12-22 08:19:42 -0500

    Add files via upload
    Adds all translated text for a given language on same line

--- wikt_scrape_en.py
+++ wikt_scrape_en.py
@@ -4,16 +4,15 @@ import re
 import sys # be able to add arguments
 import requests
 from bs4 import BeautifulSoup
-
 # add underscores to string arguments to complete URL
 def get_search_term():
     search_term = ""
     for el in range(len(sys.argv) - 1):
         # add underscores    
         search_term += (sys.argv[el + 1] + "_")
-        # delete trailing underscore
-    search_term = search_term.rstrip(search_term[-1])
-    return search_term
+        # delete trailing underscore and return URL
+    return search_term[:-1]
+# check for a link to another translation page
 def check_linked_translations():
     pseudo_navframe = soup.find("div", class_="pseudo NavFrame")
     if pseudo_navframe:
@@ -22,12 +21,9 @@ def check_linked_translations():
         if link: 
             URL = "https://en.wiktionary.org" + link.get("href")
             return(URL)
-
-
-
+# grab translation from selected languages for each translation table
 def get_translations():
     transl = soup.find_all("div", {"id": re.compile(r'Translations-*')})
-    #print(len(transl))
     # languages whose list items will be selected
     langs = ["fr", "de", "es", "ar", "fa"] 
     # check for list item for each language
@@ -35,25 +31,24 @@ def get_translations():
         # get English meaning of each definition
         print( "\n" + x.find("div", class_="NavHead").text)
         for el in langs:
-            # print translation for each language, if there
+            # print translation for each meaning/language, if present
             spans =x.find_all("span", lang=el)
             if spans:
+                span_texts = ""
                 for span in spans:
-                    print("    " + el + ": " +span.text)
-# fill URL from arguments string
+                   span_texts += span.text + ", "
+                #span_texts = span_texts.rstrip(span_texts[-4])
+                print("    " + el + ": " + span_texts[:-2])
+# fill URL from arguments
 URL = "https://en.wiktionary.org/wiki/" + get_search_term() + "#English"
 page = requests.get(URL)
 # soup it
 soup = BeautifulSoup(page.content, "html.parser")
-
-# Needs to follow hrefs for pages like "book" that lead to a different page.  If used as function, href can be checked for and the function can be called again on that link.  
-
 get_translations()
+# repeat process for any linked pages for a given word
 if check_linked_translations():
     URL = check_linked_translations()
     page = requests.get(URL)
     # soup it
     soup = BeautifulSoup(page.content, "html.parser")
     get_translations()
-
-#get_translations()
